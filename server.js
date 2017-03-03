a03256a2c255f57a55f91606476119cbab4fbe6d
manzoor11 <manzoor11@gmail.com>
2017-03-04 00:14:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config = {
+    user: 'manzoor11',
+    database: 'manzoor11',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -100,6 +108,25 @@ function createTemplate(data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config)
+
+var server = http.createServer(function(req, res) {
+    //make a select request
+    //return response with a result
+    pool.query('SELECT * FROM test', function (err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
+app.get('/test-db', function (req, res) {
+    //make a select request and return respons with response
+})
+
 app.get(':/articleName', function(req, res)
 {    // articleName = article-one
     //articles[articleName] = {} content object for article one
