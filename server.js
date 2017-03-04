9476a478eafa223d2d5d6c54bb481cb2f8cb7008
manzoor11 <manzoor11@gmail.com>
2017-03-04 20:06:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/test-db', function (req, res) {
     });
 });
 
-app.get('/articles/articleName', function(req, res)
+app.get('/articles/:articleName', function(req, res)
 {    // articleName = article-one
     //articles[articleName] = {} content object for article one
    
