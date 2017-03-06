ce36ca17f70fa786ccce5c6c2f08777503a3a151
manzoor11 <manzoor11@gmail.com>
2017-03-07 00:18:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,6 +56,33 @@ submit.onclick = function() {
 };
 
 
+
+// counter code
+
+var button = document.getElementById("counter");
+button.onclick = function() {
+
+  
+  // creat a request to counter end point
+  var request = new XMLHttpRequest();
+  
+  //capture the response
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        // take some action 
+        if(request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+     // not done yet
+  };
+    // make request
+    request.open('GET', 'http://manzoor11.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
+
 /*
 //submit Name
 var submit = document.getElementById('submit_btn');
