a8ca84d397ebf73c07a4f0f84773457d7c41f932
manzoor11 <manzoor11@gmail.com>
2017-03-07 00:57:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,11 +65,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
 function hash(input, salt) {
     //how do we create hash?
-    var hashed = crypto.pbkdf25sync(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
+
+app.get('/hash/:input', function(req,res) {
+    var hashedString = hash(req.params.input);
+    res.send(hashedString);
+});
 
 app.post('/create-user', function(req,res) {
     //username, password
